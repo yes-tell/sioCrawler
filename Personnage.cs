cded945c5a846030b0a0d30dcda1601f5989a87a
yes-tell <estelledivin8@gmail.com>
2020-09-30 09:40:34 +0200

    derniere modif

--- Personnage.cs
+++ Personnage.cs
@@ -1,8 +1,10 @@
-Personnage{
-void frapper(Personnage personnage, int force){
+class Personnage{
+public void Frapper(Personnage personnage, int force){
 
-}
-    public Lieu Regarder(Direction direction, int distance) {
+    }
+       public Lieu Regarder(Direction direction, int distance) {
 
-        }
+           public void Deplacer (Direction direction) {
+
+    }  
 }
